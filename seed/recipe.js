be12b3538394065f35b562c370102cb57f7c3fee
jonclindner <jonathan.c.lindner@gmail.com>
2023-03-31 15:12:16 -0600

    added second recipe to seed file

--- seed/recipe.js
+++ seed/recipe.js
@@ -37,6 +37,37 @@ const main = async () => {
       image:
         'https://preppykitchen.com/wp-content/uploads/2017/03/Chicken-Piccata-Feature.jpg',
       video: 'https://www.youtube.com/watch?v=ArQ567m3oVo&t=23s'
+    },
+    {
+      name: 'Panang Curry',
+      description: `Panang Gai (พะแนงไก่) or Panang curry with chicken, is a traditional Thai recipe with red curry paste, chicken and coconut milk. This curry originates from the Panang, in the central region of Thailand. It has the most prominent zesty and nutty flavors of Kaffir lime and Peanuts. This orangish curry absolutely looks delicious and mouth-watering. It is so simple and can be prepared in just 20 mins if you have the homemade curry paste ready with you. The only difference between the red curry paste and the Panang curry paste would be the roasted peanut, making the curry thick and creamy. Trust me you are going to love this curry and it's definitely worth a try.In this recipe, you will understand how to make the curry paste from scratch. We usually prefer fresh homemade Panang curry paste as it's the best way to get the fresh flavors of the curry.`,
+      instructions:
+        'In a saute pan or a wok over medium-high heat, reduce ½ cup coconut milk until thickened and creamy.2 Stir in curry paste and reduce the heat to medium-low. Cook the paste for a few minutes, stirring constantly, until coconut oil separates away from the paste.3 Add palm sugar and kaffir lime leaves and cook for a minute or so until the sugar is dissolved.4 Add chicken and quickly toss it with the curry paste, Once the chicken is about 50% cooked, add the remaining coconut milk and stir for 30 more seconds or just until the chicken is fully cooked. If it looks too dry, you can add a splash of water at this point. 5 Add some fish sauce and cook the curry for 5 mins. 6 Add in red peppers and turn off the heat. 7 Garnish with julienned kaffir lime leaves and more red peppers as desired. 8 Serve with jasmine rice, enjoy!',
+      prepTime: '10 minutes',
+      totalTime: '30 minutes',
+      ingredient1: 'Dried Red Chilies',
+      ingredient2: 'Shallots',
+      ingredient3: 'Lemongrass Stalk',
+      ingredient4: 'Garlic',
+      ingredient5: 'Kaffir Lime Zest',
+      ingredient6: 'Galangal',
+      ingredient7: 'Shrimp Paste',
+      ingredient8: 'Cumin and Coriander Seeds',
+      ingredient9: 'Roasted Peanuts',
+      ingredient10: 'Coconut Milk',
+      ingredient11: 'White Pepper Powder',
+      ingredient12: 'Panang Curry Paste',
+      ingredient13: 'Palm Sugar',
+      ingredient14: 'Kaffir Lime Leaves',
+      ingredient15: 'Chicken Thigh Fillets',
+      ingredient16: 'Fish Sauce',
+      ingredient17: '',
+      ingredient18: '',
+      ingredient19: '',
+      ingredient20: '',
+      image:
+        'https://satyamskitchen.com/wp-content/uploads/2021/05/website-3.jpg',
+      video: 'youtube.com/watch?v=dYZHzK7YfYI'
     }
   ]
 
