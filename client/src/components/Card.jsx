1ba3fec0918f882accaa2545ff37d71a8e6b5833
jonclindner <jonathan.c.lindner@gmail.com>
2023-04-01 09:49:07 -0600

    added content to card component" "

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,116 @@
+import { AiOutlineEdit } from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom'
+import { TfiClose } from 'react-icons/tfi'
+import axios from 'axios'
+
+const Card = ({
+  id,
+
+  image,
+
+  name,
+  reviews,
+  totalTime,
+  video,
+  userId,
+  getRecipes
+}) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/viewRecipe/${id}`)
+  }
+
+  const handleDelete = async () => {
+    await axios.delete(`/api/recipe/${id}`)
+    getRecipes()
+  }
+
+  const handleEdit = () => {
+    navigate(`/editRecipe/${id}`)
+  }
+
+  return (
+    <div className="pt-8 pb-16 sm:pt-8 sm:pb-8 xl:pb-8 rounded-2xl m-4 mt-32 ">
+      <div className="bg-black pb-20 sm:pb-24 xl:pb-0 rounded-2xl">
+        <div className="mx-auto flex max-w-7xl flex-col items-center gap-y-10 gap-x-8 px-6 sm:gap-y-8 lg:px-8 xl:flex-row xl:items-stretch">
+          <div className="-mt-8 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
+            <div className="relative aspect-[2/1] h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
+              <img
+                className="absolute inset-0 h-full w-full rounded-2xl object-cover shadow-2xl"
+                src={image}
+                alt={name}
+              />
+            </div>
+          </div>
+          <div className="w-full max-w-2xl xl:max-w-none xl:flex-auto xl:py-24 xl:px-16">
+            {userId === sessionStorage.getItem('user') && (
+              <div className="flex gap-4">
+                <TfiClose
+                  size={20}
+                  onClick={handleDelete}
+                  className="text-white cursor-pointer"
+                />
+
+                <AiOutlineEdit
+                  size={22}
+                  onClick={handleEdit}
+                  className="text-white cursor-pointer"
+                />
+              </div>
+            )}
+            <figure className="relative isolate pt-6 sm:pt-12">
+              <blockquote className="text-xl font-semibold leading-8 text-white sm:text-2xl sm:leading-9 ">
+                <p>{name}</p>
+                <div className="mt-1 text-gray-400">location</div>
+              </blockquote>
+              <div className="flex flex-row flex-wrap gap-10">
+                <figcaption className="mt-8 text-base mr-4 w-32">
+                  <div className="font-semibold text-white">Height</div>
+                  <div className="mt-1 text-gray-400">height</div>
+                </figcaption>
+                <figcaption className="mt-8 text-base mr-4 w-32">
+                  <div className="font-semibold text-white">Time</div>
+                  <div className="mt-1 text-gray-400">{totalTime}</div>
+                </figcaption>
+                <figcaption className="mt-8 text-base mr-4 w-32">
+                  <div className="font-semibold text-white">Feet</div>
+                  <div className="mt-1 text-gray-400">length</div>
+                </figcaption>
+                <figcaption className="mt-8 text-base mr-4 w-32">
+                  <div className="font-semibold text-white">Video</div>
+                  <div className="mt-1 text-gray-400">
+                    <a
+                      className="text-indigo-500"
+                      href={video}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Click to watch
+                    </a>
+                  </div>
+                </figcaption>
+                <figcaption className="mt-8 text-base mr-4 w-32">
+                  <div className="font-semibold text-white">
+                    Comments ({reviews.length})
+                  </div>
+                  <div
+                    onClick={handleClick}
+                    className="mt-1 text-gray-400 cursor-pointer text-indigo-500"
+                  >
+                    See more
+                  </div>
+                </figcaption>
+                <figcaption className="mt-8 text-base mr-4 w-32">
+                  {/* <BsHeart className="text-white cursor-pointer" size={30} /> */}
+                </figcaption>
+              </div>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,76 @@
+import { GiCook } from 'react-icons/tb'
+import jwt_decode from 'jwt-decode'
+import { useEffect } from 'react'
+import Home from './Home'
+import axios from 'axios'
+/*global google*/
+
+const SignIn = ({ setUser }) => {
+  const handleCallBackResponse = async (response) => {
+    let userObject = jwt_decode(response.credential)
+    try {
+      const res = await axios.post(`/api/user`, {
+        name: userObject.name,
+        email: userObject.email,
+        picture: userObject.picture
+      })
+      setUser(res.data.user)
+      sessionStorage.setItem('user', res.data.user._id)
+      sessionStorage.setItem('name', res.data.user.name)
+      sessionStorage.setItem('picture', res.data.user.picture)
+    } catch (err) {
+      if (err.response.status === 500) {
+        try {
+          let email = { email: userObject.email }
+          let res = await axios.post('/api/login', email)
+          if (res.data.message === 'Login successful') {
+            setUser(res.data.user)
+            sessionStorage.setItem('user', res.data.user._id)
+            sessionStorage.setItem('name', res.data.user.name)
+            sessionStorage.setItem('picture', res.data.user.picture)
+          }
+        } catch (e) {}
+      }
+    }
+  }
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallBackResponse
+    })
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large'
+    })
+  }, [])
+
+  return !sessionStorage.getItem('user') ? (
+    <div className="h-screen">
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <GiCook className="mx-auto h-12 w-auto text-indigo-600" />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to browse&nbsp;
+              <span className="text-indigo-600 hover:text-indigo-700">
+                Coaster
+              </span>
+            </h2>
+          </div>
+          <form
+            className="mt-8 space-y-6 flex items-center justify-center"
+            action="#"
+            method="POST"
+          >
+            <div id="signInDiv"></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Home />
+  )
+}
+
+export default SignIn
